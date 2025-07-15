f0460863b5649a726e4f9253ca0143ae7123535a
jupitorSendsme <jupitorsendsme@gmail.com>
2025-07-15 22:53:14 +0900

    web-module setting

--- web-module/server/index.js
+++ web-module/server/index.js
@@ -1,15 +1,35 @@
-import express from 'express'
-import { apply } from 'vike-server/express'
+import express from "express";
+import { createDevMiddleware, renderPage } from "vike/server";
 
-function startServer() {
-  const app = express()
-  
-  apply(app)
-  
-  const port = process.env.PORT || 3000
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
-  })
+const isProduction = process.env.NODE_ENV === "production";
+
+const root = new URL("..", import.meta.url).pathname;
+
+async function startServer() {
+    const app = express();
+
+    if (isProduction) {
+        app.use(express.static(`${root}/dist/client`));
+    } else {
+        const { devMiddleware } = await createDevMiddleware({ root });
+        app.use(devMiddleware);
+    }
+
+    app.get(/.*/, async (req, res, next) => {
+        const pageContextInit = {
+            urlOriginal: req.originalUrl,
+        };
+        const pageContext = await renderPage(pageContextInit);
+        const { httpResponse } = pageContext;
+        if (!httpResponse) return next();
+        const { body, statusCode, headers } = httpResponse;
+        const contentType = headers?.["content-type"] || "text/html";
+        res.status(statusCode).type(contentType).send(body);
+    });
+
+    const port = process.env.PORT || 3030;
+    app.listen(port);
+    console.log(`Server running at http://localhost:${port}`);
 }
 
-startServer()
+startServer();
